616ddb4a2bdd179eabad5a923d050223dce959ed
Andrey Roshchupkin <reivrn@gmail.com>
2023-07-27 11:29:52 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+import fetch from "node-fetch";
+
+// Function to make the GET request
+const makeGetRequestToDocInfo = () => {
+  fetch("https://docinfo.onrender.com")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+      console.log("GET request to DocInfo successful");
+    })
+    .catch((error) => {
+      console.error("Error making GET request to DocInfo:", error.message);
+    });
+};
+
+// Function to make the GET request every 5 minutes
+const sendRequestEveryFiveMinutes = () => {
+  makeGetRequestToDocInfo();
+  setTimeout(sendRequestEveryFiveMinutes, 5 * 60 * 1000); // 5 minutes in milliseconds
+};
+
+// Start sending the GET requests
+sendRequestEveryFiveMinutes();
