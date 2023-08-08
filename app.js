1ac11924e6ab7b99d657ffab2aefa593ea1c7e78
Andrey Roshchupkin <reivrn@gmail.com>
2023-08-08 18:50:31 +0400

    add get request to mock server

--- app.js
+++ app.js
@@ -14,9 +14,23 @@ const makeGetRequestToDocInfo = () => {
     });
 };
 
+const makeGetRequestToDocInfoMockServer = () => {
+  fetch("https://mock-server-docinfo.onrender.com")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+      console.log("GET request to DocInfo successful");
+    })
+    .catch((error) => {
+      console.error("Error making GET request to DocInfo:", error.message);
+    });
+};
+
 // Function to make the GET request every 5 minutes
 const sendRequestEveryFiveMinutes = () => {
   makeGetRequestToDocInfo();
+  makeGetRequestToDocInfoMockServer();
   setTimeout(sendRequestEveryFiveMinutes, 5 * 60 * 1000); // 5 minutes in milliseconds
 };
 
